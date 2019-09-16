92f00b09f9a3f44f548445c9e53a627ef82b7d73
sergio087 <sergio.nascimbene@despegar.com>
2019-09-15 21:30:48 -0300

    proyecto base maven java web

---
+++ src/main/java/ar/edu/utn/frba/dds/App.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
