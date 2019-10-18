a8b269bfbc0167827bb5ae73a6a5b033099daafa
sergio087 <sergio.nascimbene@despegar.com>
2019-10-17 22:13:13 -0300

    ejemplo exception handler con dominio

--- src/main/java/ar/edu/utn/frba/dds/controller/ExceptionHandlersConfig.java
+++ src/main/java/ar/edu/utn/frba/dds/controller/ExceptionHandlersConfig.java
@@ -1,8 +1,15 @@
 package ar.edu.utn.frba.dds.controller;
 
+import ar.edu.utn.frba.dds.domain.exception.OfertaInvalidaException;
+import org.eclipse.jetty.http.HttpStatus;
+import spark.Spark;
+
 public class ExceptionHandlersConfig {
 
     public static void exceptionHandlers() {
 
+        Spark.exception(OfertaInvalidaException.class, (exception, request, response) -> {
+            response.status(HttpStatus.BAD_REQUEST_400);
+        } ) ;
     }
 }
